adcdb711f0fa95301d8dcc93fabd0ed7c0105b41
David Herbert <davidherbertsb@gmail.com>
2023-07-27 02:42:08 -0300

    Atualizações no código

--- barramentos.vhd
+++ barramentos.vhd
@@ -13,8 +13,8 @@ entity InstructionFetchDecodePipeline is
         PC_out         : out std_logic_vector(31 downto 0);
         Instruction_out: out std_logic_vector(31 downto 0);
         PC_PLUS_4_out  : out std_logic_vector(31 downto 0);
-        rs1_out        : out std_logic_vector(4 downto 0);
-        rs2_out        : out std_logic_vector(4 downto 0);
+        ro1_out        : out std_logic_vector(4 downto 0);
+        ro2_out        : out std_logic_vector(4 downto 0);
         rd_out         : out std_logic_vector(4 downto 0)
     );
 end entity;
@@ -27,8 +27,8 @@ begin
             PC_out              <= PC_in;
             Instruction_out     <= Instruction_in;
             PC_PLUS_4_out       <= PC_PLUS_4_in;
-            rs1_out             <= Instruction_in(19 downto 15);
-            rs2_out             <= Instruction_in(24 downto 20);
+            ro1_out             <= Instruction_in(19 downto 15);
+            ro2_out             <= Instruction_in(24 downto 20);
             rd_out              <= Instruction_in(11 downto 7);
         end if;
     end process;
@@ -39,16 +39,16 @@ entity InstructionDecodeExecutePipeline is
     port (
         clk             : in std_logic;
         PC_in           : in std_logic_vector(31 downto 0);
-        rs1_in          : in std_logic_vector(31 downto 0);
-        rs2_in          : in std_logic_vector(31 downto 0);
+        ro1_in          : in std_logic_vector(31 downto 0);
+        ro2_in          : in std_logic_vector(31 downto 0);
         rd_in           : in std_logic_vector(4 downto 0);
         imm_in          : in std_logic_vector(31 downto 0);
         instr_in        : in std_logic_vector(31 downto 0);
         PC_PLUS_4_in    : in std_logic_vector(31 downto 0);
 
         PC_out          : out std_logic_vector(31 downto 0);
-        rs1_out         : out std_logic_vector(31 downto 0);
-        rs2_out         : out std_logic_vector(31 downto 0);
+        ro1_out         : out std_logic_vector(31 downto 0);
+        ro2_out         : out std_logic_vector(31 downto 0);
         rd_out          : out std_logic_vector(4 downto 0);
         imm_out         : out std_logic_vector(31 downto 0);
         instr_out       : out std_logic_vector(31 downto 0);
@@ -79,8 +79,8 @@ begin
     begin
         if rising_edge(clk) then
             PC_out           <= PC_in;
-            rs1_out          <= rs1_in;
-            rs2_out          <= rs2_in;
+            ro1_out          <= ro1_in;
+            ro2_out          <= ro2_in;
             rd_out           <= rd_in;
             imm_out          <= imm_in;
             instr_out        <= instr_in;
@@ -110,7 +110,7 @@ entity ExecuteMemoryPipeline is
         imm_in          : in std_logic_vector(31 downto 0);
         PC_PLUS_4_in    : in std_logic_vector(31 downto 0);
 
-        PC_PLUS_Imm_out : out std_logic_vector(31 downto 0);
+        PC_plus_Imm_out : out std_logic_vector(31 downto 0);
         zero_out        : out std_logic;
         ro2_out         : out std_logic_vector(31 downto 0);
         rd_out          : out std_logic_vector(4 downto 0);
@@ -139,7 +139,7 @@ begin
     process (clk)
     begin
         if rising_edge(clk) then
-            PC_PLUS_Imm_out <= PC_plus_Imm_in;
+            PC_plus_Imm_out <= PC_plus_Imm_in;
             zero_out        <= zero_in;
             ro2_out         <= ro2_in;
             rd_out          <= rd_in;
